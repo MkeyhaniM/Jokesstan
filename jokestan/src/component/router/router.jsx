57b4db0efc0b5657657ed5eff9b416da56b1ed96
mkeyhanim <mkeyhanideve@gmail.com>
2023-04-10 18:34:13 -0700

    Create route

--- jokestan/src/component/router/router.jsx
+++ jokestan/src/component/router/router.jsx
@@ -1,15 +1,27 @@
-import {useRoutes} from 'hookrouter';
-import {Mainpage} from "../pages/mainpage/mainpage";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate
+} from "react-router-dom";
+
+import {Home} from "../pages/mainpage/home";
 import {About} from "../pages/about/about";
 import {NotFoundPage} from "../pages/notFoundPage/NotFoundPage";
 import {Sign} from "../pages/sign-in/sign";
+import {Layout} from "../pages/layout/layout";
 
-const routes = {
-    '/': () => <Mainpage/>,
-    '/about': () => <About/>,
-    '/sign': () => <Sign/>,
-}
-export const MyApp = () => {
-    const routeResult = useRoutes(routes);
-    return routeResult || <NotFoundPage/>;
+export function RouterJokestan() {
+    return (
+        <Router>
+            <Routes>
+                <Route path='/home' element={<Navigate to='/'/>} />
+                <Route path='/sign' element={<Sign/>}/>
+                <Route path='/not-found' element={<NotFoundPage/>}/>
+                <Route path='/about' element={<About/>}/>
+                <Route path='/start' element={<Layout/>}/>
+                <Route path='/' element={<Home/>}/>
+            </Routes>
+        </Router>
+    );
 }
