bc5a4477aca4ba3b58bfac654b618051b2b0918d
mkeyhanim <mkeyhanideve@gmail.com>
2023-04-18 15:50:50 -0700

    Parent all of sentence

--- jokestan/src/component/pages/layout/createRticles.jsx
+++ jokestan/src/component/pages/layout/createRticles.jsx
@@ -1,118 +1,64 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
-import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import { Container } from "@mui/material";
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
+import Box from "@mui/material/Box";
+import Fab from "@mui/material/Fab";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ContentCopyRounded from "@mui/icons-material/ContentCopyRounded";
+import CopyRate from "@mui/icons-material/StarRateRounded";
+import Typography from "@mui/material/Typography";
+import { makeStyles } from "@mui/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const useStyle = makeStyles({
+  buttonOptions: {
+
+    padding: "40px",
+  },
+});
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Kanit", "serif"].join(","),
+  },
+});
 
 export default function CreateRticles() {
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
+  const classes = useStyle();
   return (
-    <Container maxWidth="sm">
-      <Card sx={{ maxWidth: 345 }}>
-        <CardHeader
-          avatar={
-            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-              R
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title="Shrimp and Chorizo Paella"
-          subheader="September 14, 2016"
-        />
-        <CardMedia
-          component="img"
-          height="194"
-          image="/static/images/cards/paella.jpg"
-          alt="Paella dish"
-        />
-        <CardContent>
-          <Typography variant="body2" color="text.secondary">
-            This impressive paella is a perfect party dish and a fun meal to
-            cook together with your guests. Add 1 cup of frozen peas along with
-            the mussels, if you like.
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <IconButton aria-label="add to favorites">
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton aria-label="share">
-            <ShareIcon />
-          </IconButton>
-          <ExpandMore
-            expand={expanded}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label="show more"
-          >
-            <ExpandMoreIcon />
-          </ExpandMore>
-        </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
-          <CardContent>
-            <Typography paragraph>Method:</Typography>
-            <Typography paragraph>
-              Heat 1/2 cup of the broth in a pot until simmering, add saffron
-              and set aside for 10 minutes.
+    <Box
+      sx={{
+        "& > :not(style)": { m: 1 },
+        maxWidth: 900,
+        border: 3,
+        boxShadow: "16px 19px 13px -3px rgba(0,0,0,0.1)",
+        borderRadius: 3,
+        margin: "5rem auto",
+      }}
+    >
+      <Box>
+        <Box className={classes.buttonOptions}>
+          <ThemeProvider theme={theme}>
+            <Typography align="left">
+              palette color objects are provided, they will replace the default
+              onesThe palette color value can either be a color object, or an
+              object with one or more of the keys specified by the following
+              TypeScript interface
             </Typography>
-            <Typography paragraph>
-              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-              over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-              stirring occasionally until lightly browned, 6 to 8 minutes.
-              Transfer shrimp to a large plate and set aside, leaving chicken
-              and chorizo in the pan. Add pimentón, bay leaves, garlic,
-              tomatoes, onion, salt and pepper, and cook, stirring often until
-              thickened and fragrant, about 10 minutes. Add saffron broth and
-              remaining 4 1/2 cups chicken broth; bring to a boil.
-            </Typography>
-            <Typography paragraph>
-              Add rice and stir very gently to distribute. Top with artichokes
-              and peppers, and cook without stirring, until most of the liquid
-              is absorbed, 15 to 18 minutes. Reduce heat to medium-low, add
-              reserved shrimp and mussels, tucking them down into the rice, and
-              cook again without stirring, until mussels have opened and rice is
-              just tender, 5 to 7 minutes more. (Discard any mussels that
-              don&apos;t open.)
-            </Typography>
-            <Typography>
-              Set aside off of the heat to let rest for 10 minutes, and then
-              serve.
-            </Typography>
-          </CardContent>
-        </Collapse>
-      </Card>
-    </Container>
+          </ThemeProvider>
+        </Box>
+        <Fab margin={5} aria-label="like" variant="extended">
+          <FavoriteIcon sx={{ mr: 1 }} fontSize="large" />
+          Favorite
+        </Fab>
+        <Fab margin={2} aria-label="like" variant="extended">
+          <ContentCopyRounded sx={{ mr: 1 }} fontSize="large" />
+          CopyIcon
+        </Fab>
+        <Fab margin={2} aria-label="like" variant="extended">
+          <CopyRate sx={{ mr: 1 }} color="warning1" fontSize="large" />
+          Rate
+        </Fab>
+      </Box>
+    </Box>
   );
 }
