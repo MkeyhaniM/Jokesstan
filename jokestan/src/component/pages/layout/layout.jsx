fcdafbba13da15c0a215f4bf63981b7c508dfb7d
mkeyhanim <mkeyhanideve@gmail.com>
2023-04-18 15:52:22 -0700

    Grid between Loop and Subject component

--- jokestan/src/component/pages/layout/layout.jsx
+++ jokestan/src/component/pages/layout/layout.jsx
@@ -1,14 +1,37 @@
 import "../../../assets/style/start.css";
 import CreateRticles from "./createRticles";
 import Subjects from "./subjects";
+import Loop from "./loop";
+
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  // padding: theme.spacing(1),
+  textAlign: "center",
+  // color: theme.palette.text.secondary,
+}));
 
 export function Layout() {
   return (
-    <>
-      <section id="startPage">
-        <Subjects />
-        <CreateRticles />
-      </section>
-    </>
+    <Box>
+      <Grid container  direction="row" justifyContent="center"  alignItems="stretch">
+        <Grid item xs={6} md={2}>
+          <Item>
+            <Subjects id='VhHeight'/>
+          </Item>
+        </Grid>
+        <Grid id='mAuto' item xs={6} md={10}>
+          <Item>
+            <Loop/>
+          </Item>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
