c22a83dea25bcc0516ff7693482e555c1cfb20ca
mkeyhanim <mkeyhanideve@gmail.com>
2023-04-20 18:03:23 -0700

    First request for generate all types

--- jokestan/src/component/pages/layout/loop.jsx
+++ jokestan/src/component/pages/layout/loop.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CreateRticles from "./createRticles";
 import Container from "@mui/material/Container";
+import request from "../../ax-jok/Ax-jokestan";
 
-const arr = [1,2,3,4,5,6,7,8,9,10]
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 export default function Loop() {
+  const [state, setState] = useState([]);
+  console.log(state);
+
+  useEffect(() => {
+    request().then((response) => {
+      // setState(response.data);
+    });
+
+    return () => {
+      function Looping() {
+        return arr.map((e, index) => {
+          return <CreateRticles key={e + index} />;
+        });
+      }
+    };
+  }, [state, request()]);
+
   function Looping() {
-      return arr.map((e,index) => {
-        console.log('hello');
-        return <CreateRticles key={e+index}/>
+    return arr.map((e, index) => {
+      return <CreateRticles key={e + index} />;
     });
   }
 
-  return <Container><Looping /></Container>;
+  return <Container>{/* <Looping/> */}</Container>;
 }
