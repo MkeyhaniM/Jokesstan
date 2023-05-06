fefdfeb31b3aeb09e36ca49ff24a35ee9a3a1339
mkeyhanim <mkeyhanideve@gmail.com>
2023-05-06 08:10:52 -0700

    Send a props for change between subjects and list of favour

--- jokestan/src/component/pages/layout/layout.jsx
+++ jokestan/src/component/pages/layout/layout.jsx
@@ -1,13 +1,14 @@
 import "../../../assets/style/start.css";
-import CreateRticles from "./createRticles";
 import Subjects from "./subjects";
 import Loop from "./loop";
 
 import * as React from "react";
+import { useState, useEffect } from "react";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import CreateFavourList from "./createFavourList";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -16,17 +17,50 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export function Layout() {
+  const [switchS, setSwitchS] = useState(true);
+  const [getSub, setGetSub] = useState();
+
+  function switchState() {
+    if (switchS) {
+      setSwitchS(false);
+    } else {
+      setSwitchS(true);
+    }
+  }
+
+  useEffect(() => {
+    if (getSub && !switchS) {
+      switchState();
+    }
+  }, [getSub]);
+
+  function gettingSubject(e) {
+    setGetSub(e);
+  }
+
   return (
     <Box>
-      <Grid container  direction="row">
-        <Grid item xs={6} md={2} sx={{backgroundColor:'#C9A7EB'}} maxHeight={1010} >
+      <Grid container direction="row">
+        <Grid
+          item
+          xs={6}
+          md={2}
+          sx={{ backgroundColor: "#C9A7EB" }}
+          maxHeight={1010}
+        >
           <Item>
-            <Subjects id='VhHeight'/>
+            <Subjects id="VhHeight" c={switchState} g={gettingSubject} />
           </Item>
         </Grid>
-        <Grid id='mAuto' item xs={6} md={10} sx={{backgroundColor:'#443C68'}}>
-          <Item sx={{backgroundColor:'#443C68'}}>
-            <Loop/>
+        <Grid
+          id="mAuto"
+          item
+          xs={6}
+          md={10}
+          sx={{ backgroundColor: "#443C68" }}
+        >
+          <Item sx={{ backgroundColor: "#443C68" }}>
+            {switchS ? <Loop subject={getSub} /> : <CreateFavourList />}
           </Item>
         </Grid>
       </Grid>
