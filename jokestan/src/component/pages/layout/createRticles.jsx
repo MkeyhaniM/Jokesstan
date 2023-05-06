057c5f2630e5c55ad8e55da19f96c34ea6a24f43
mkeyhanim <mkeyhanideve@gmail.com>
2023-05-06 08:09:12 -0700

    Get a props {idSen}

--- jokestan/src/component/pages/layout/createRticles.jsx
+++ jokestan/src/component/pages/layout/createRticles.jsx
@@ -11,7 +11,7 @@ import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
 import { makeStyles } from "@mui/styles";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import  addFavour  from "./addFavour";
+import addFavour from "./addFavour";
 
 const useStyle = makeStyles({
   buttonOptions: {
@@ -32,10 +32,11 @@ const theme2 = createTheme({
   },
 });
 
-export default function CreateRticles({ sentence, type }) {
+export default function CreateRticles({sentence, type, idSen}) {
   const classes = useStyle();
   return (
     <Box
+      id={idSen}
       sx={{
         "& > :not(style)": { m: 1 },
         maxWidth: 900,
@@ -66,7 +67,7 @@ export default function CreateRticles({ sentence, type }) {
                   aria-label="like"
                   variant="extended"
                   color="default"
-                  onClick={() => addFavour(sentence, type)}
+                  onClick={() => addFavour(sentence, type, idSen)}
                 >
                   <FavoriteIcon
                     sx={{ mr: 1 }}
