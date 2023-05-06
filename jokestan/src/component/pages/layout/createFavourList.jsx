826ece4e56c997b5b2887a18a62bf0e486691d95
mkeyhanim <mkeyhanideve@gmail.com>
2023-05-06 08:07:34 -0700

    There is make list of favour

---
+++ jokestan/src/component/pages/layout/createFavourList.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+
+import Box from "@mui/material/Box";
+import Fab from "@mui/material/Fab";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ContentCopyRounded from "@mui/icons-material/ContentCopyRounded";
+import CopyRate from "@mui/icons-material/StarRateRounded";
+import Typography from "@mui/material/Typography";
+import FormatQuoteIcon from "@mui/icons-material/FormatQuote";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import { makeStyles } from "@mui/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const useStyle = makeStyles({
+  buttonOptions: {
+    padding: "40px",
+  },
+});
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Ubuntu", "serif"].join(","),
+  },
+});
+
+const theme2 = createTheme({
+  typography: {
+    fontFamily: ["Acme", "serif"].join(","),
+    fontSize: 17,
+  },
+});
+
+export default function CreateFavourList() {
+  const classes = useStyle();
+  return (
+    <Box
+      sx={{
+        "& > :not(style)": { m: 1 },
+        maxWidth: 900,
+        border: 2,
+        borderColor: "#E384FF",
+        boxShadow: "4px 4px 10px 0 #FFA3FD",
+        borderRadius: 3,
+        margin: "5rem auto",
+      }}
+    >
+      <Box>
+        <Box className={classes.buttonOptions}>
+          <ThemeProvider theme={theme}>
+            <Typography color={"snow"} fontSize={20} align="left">
+              {}
+            </Typography>
+          </ThemeProvider>
+        </Box>
+        <Box
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"space-around"}
+        >
+          <Box display={"flex"}>
+            <Box marginX={1}>
+              <ThemeProvider theme={theme2}>
+                <Fab
+                  aria-label="like"
+                  variant="extended"
+                  color="default"
+                >
+                  <FavoriteIcon
+                    sx={{ mr: 1 }}
+                    fontSize="medium"
+                    color="error"
+                  />
+                  Favorite
+                </Fab>
+              </ThemeProvider>
+            </Box>
+            <Box marginX={1}>
+              <ThemeProvider theme={theme2}>
+                <Fab aria-label="like" variant="extended" color="default">
+                  <ContentCopyRounded
+                    sx={{ mr: 1 }}
+                    fontSize="medium"
+                    color="info"
+                  />
+                  Copy
+                </Fab>
+              </ThemeProvider>
+            </Box>
+            <Box marginX={1}>
+              <ThemeProvider theme={theme2}>
+                <Fab aria-label="like" variant="extended" color="default">
+                  <CopyRate sx={{ mr: 1 }} fontSize="medium" color="warning" />
+                  Rate
+                </Fab>
+              </ThemeProvider>
+            </Box>
+          </Box>
+          <Box alignItems={"end"}>
+            <Stack direction="row" spacing={1} marginY={2}>
+              <Chip
+                icon={<FormatQuoteIcon />}
+                // label={type}
+                color="primary"
+                variant="filled"
+              />
+            </Stack>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
