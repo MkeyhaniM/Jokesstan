419d3750d943fa3e1ccc2be89d44ace6105a4401
mkeyhanim <mkeyhanideve@gmail.com>
2023-05-21 09:57:03 -0700

    Improve the api

--- jokestan/src/component/ax-jok/Ax-jokestan.js
+++ jokestan/src/component/ax-jok/Ax-jokestan.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 // axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
  
-axios.defaults.baseURL = 'http://localhost:1010';
+// axios.defaults.baseURL = 'http://localhost:1010';
 
 const request = async () => {
-  return axios.get(`/jokestanWord/`).then((req) => {
+  return axios.get(`http://localhost:1010/jokestanWord/`).then((req) => {
     return req.data;
   });
 };
