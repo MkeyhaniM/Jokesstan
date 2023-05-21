ee1768fbc5fbfea8c5d482e9fce1a29014dbc80c
mkeyhanim <mkeyhanideve@gmail.com>
2023-05-21 10:15:48 -0700

    Server that recieves post request by client, first we must start it on commend line

--- jokestan/src/server/index.js
+++ jokestan/src/server/index.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const port = 2020;
+const port = 3030;
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
+var signUser = [];
+console.log(userInformation);
+
 app.post("/post", (req, res) => {
-    
+  const { userInformation } = req.body;
+
+  res.send({
+    success: true,
+    data: "yas",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Connected to the local server by http://localhost:${port}`);
 });
