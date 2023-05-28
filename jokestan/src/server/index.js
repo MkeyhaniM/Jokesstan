3d00153b6b7da81cb1f443f67453658b14afbbd3
mkeyhanim <mkeyhanideve@gmail.com>
2023-05-27 20:25:04 -0700

    Managed the request for client

--- jokestan/src/server/index.js
+++ jokestan/src/server/index.js
@@ -2,24 +2,34 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const port = 3030;
-
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
 var signUser = [];
-console.log(userInformation);
+
+const Jokestan = "Jokestan";
 
 app.post("/post", (req, res) => {
   const { userInformation } = req.body;
 
-  res.send({
-    success: true,
-    data: "yas",
-  });
+  if (userInformation) {
+  
+    res.send({
+      success: true,
+      data: userInformation,
+      token: [Jokestan],
+    });
+  } else {
+    res.send({
+      success: true,
+      data: userInformation,
+      token: null,
+    });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Connected to the local server by http://localhost:${port}`);
-});
+app.listen(port, () =>
+  console.log(`Connected to the local server by http://localhost:${port}`)
+);
